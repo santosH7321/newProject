7771824cef10aa5746794a1877368d3841a88370
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-06-11 16:19:46 +0530

    Clerk auth is complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-
+"use client";
+import { SignedIn, SignOutButton } from '@clerk/clerk-react';
+import { SignedOut, SignInButton } from '@clerk/nextjs';
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
       HomePage
+
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
